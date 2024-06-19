ccab5ab53314666cdd0242c28d549709a0ebd7fb
AMAN KISHORE <67307253+amankishore09@users.noreply.github.com>
2024-06-19 10:42:18 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const shopNowButton = document.getElementById('shop-now');
+const subscribeButton = document.getElementById('subscribe');
+
+shopNowButton.addEventListener('click', () => {
+    // Add your shop now functionality here
+    console.log('Shop now button clicked!');
+});
+
+subscribeButton.addEventListener('click', () => {
+    // Add your subscribe functionality here
+    console.log('Subscribe button clicked!');
+});
